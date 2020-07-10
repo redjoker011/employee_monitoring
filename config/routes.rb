c3450af977a1510ab23a2e231f21415dcffd86ac
Peter John Alvarado <peterjohn@gorated.ph>
2020-07-10 22:41:39 +0800

    [feat] Create method for deleting user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :admins, only: :index
-  resources :users, except: :create
+  resources :users, except: %i[create destroy]
   post 'create_user', to: 'users#create'
+  delete 'remove_user/:id', to: 'users#destroy', as: :remove_user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
