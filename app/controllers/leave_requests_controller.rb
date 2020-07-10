da9e75bcee1b5449657e557ea495b36aaf530a50
Peter John Alvarado <peterjohn@gorated.ph>
2020-07-11 02:36:54 +0800

    [feat] Allow creation of leave request

--- app/controllers/leave_requests_controller.rb
+++ app/controllers/leave_requests_controller.rb
@@ -2,4 +2,28 @@ class LeaveRequestsController < ApplicationController
   def index
     @requests = LeaveRequest.all
   end
+
+  def new
+    @request = LeaveRequest.new
+    @categories = LeaveRequest.categories
+                              .map { |k, _v| [k.gsub('_', ' ').titleize, k] }
+  end
+
+  def create
+    request = current_user.leave_requests.new(leave_requests_params)
+
+    if request.save
+      redirect_to user_path(current_user), notice: "Leave Request Successfully Filed"
+    else
+      flash[:alert] = request.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private 
+
+  def leave_requests_params
+    params.require(:leave_request)
+          .permit(:category, :reason, :leave_start_at, :leave_end_at)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,9 @@ module UsersHelper
   def has_leave_credits?(credits)
     !credits.zero?
   end
+
+  def to_short_date(date)
+    format = "%B %d, %Y"
+    date.strftime(format)
+  end
 end
