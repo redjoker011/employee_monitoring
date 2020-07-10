d517f2f7a3f95aed4947ec36a244fea946d5f4b4
Peter John Alvarado <peterjohn@gorated.ph>
2020-07-10 20:15:01 +0800

    [feat] Create User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+if User.all.empty?
+  User.roles.each do |k, v|
+    user = User.new(
+      email: "user_#{k}@example.com",
+      password: "hello2020",
+      role: v,
+      name: k
+    )
+    if user.save
+      print "✓"
+    else
+      print "ERROR: #{user.errors.full_messages.to_sentence}"
+      print "Aborted seeding of User model."
+      break
+    end
+
+  end
+end
