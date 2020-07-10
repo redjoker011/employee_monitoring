fe9c0be3727da5d8ab98682eb9edcbd8bcecb081
Peter John Alvarado <peterjohn@gorated.ph>
2020-07-11 01:41:35 +0800

    [feat] Add devise login redirect hook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    user = resource
+    if user.super_admin? || user.hr_staff?
+      users_path
+    else
+      user_path(user)
+    end
+  end
 end
