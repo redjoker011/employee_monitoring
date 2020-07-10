692505848ddff3fb5b64fb9709d28363d7390fc4
Peter John Alvarado <peterjohn@gorated.ph>
2020-07-11 02:50:13 +0800

    [feat] Add Leave Request Validation Validate Date Range

--- app/controllers/leave_requests_controller.rb
+++ app/controllers/leave_requests_controller.rb
@@ -1,12 +1,11 @@
 class LeaveRequestsController < ApplicationController
+  before_action :initialize_categories, only: %i[new create]
   def index
     @requests = LeaveRequest.all
   end
 
   def new
     @request = LeaveRequest.new
-    @categories = LeaveRequest.categories
-                              .map { |k, _v| [k.gsub('_', ' ').titleize, k] }
   end
 
   def create
@@ -15,15 +14,21 @@ class LeaveRequestsController < ApplicationController
     if request.save
       redirect_to user_path(current_user), notice: "Leave Request Successfully Filed"
     else
+      @request = LeaveRequest.new(leave_requests_params)
       flash[:alert] = request.errors.full_messages.to_sentence
       render :new
     end
   end
 
-  private 
+  private
 
   def leave_requests_params
     params.require(:leave_request)
           .permit(:category, :reason, :leave_start_at, :leave_end_at)
+  end
+
+  def initialize_categories
+    @categories = LeaveRequest.categories
+                              .map { |k, _v| [k.gsub('_', ' ').titleize, k] }
   end
 end
