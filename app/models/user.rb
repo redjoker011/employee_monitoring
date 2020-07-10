b315bdb185c82499245f4ef5eb2995ab502c969b
Peter John Alvarado <peterjohn@gorated.ph>
2020-07-11 01:28:56 +0800

    [feat] Display devise login error

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
   enum role: %i[employee hr_staff super_admin]
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
 
   authenticate :user do
+    root "home#index"
+
     resources :users, except: %i[create destroy]
     post 'create_user', to: 'users#create'
     delete 'remove_user/:id', to: 'users#destroy', as: :remove_user
-
-    root "home#index"
   end
 end
