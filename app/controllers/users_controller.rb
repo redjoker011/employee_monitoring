26a53ea23c0cbb2e0f3e990c0901f10960516887
Peter John Alvarado <peterjohn@gorated.ph>
2020-07-10 22:21:09 +0800

    [feat] Create User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+  before_action :initialize_roles, only: %i[new create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    unless user_params[:password] == user_params[:password]
+      flash[:alert] = 'Password Should Match'
+      render :new
+    end
+
+    user = User.new(user_params.reject! { |k| k == :confirm_password })
+    if user.save
+      redirect_to admins_path, notice: 'User Successfully Created'
+    else
+      flash[:alert] = user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    {
+      role: params[:role],
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      confirm_password: params[:confirm_password]
+    }
+  end
+
+  def initialize_roles
+    @roles = User.roles.map { |k, _v| [k, k] }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :admins, only: :index
-  resources :users
+  resources :users, except: :create
+  post 'create_user', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
