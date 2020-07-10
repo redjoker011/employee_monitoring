80efa3bafc7b0ab1583fa3730e682fba8a71d9be
Peter John Alvarado <peterjohn@gorated.ph>
2020-07-10 23:24:20 +0800

    [feat] Remove Admin Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
 
   authenticate :user do
-    resources :admins, only: :index
     resources :users, except: %i[create destroy]
     post 'create_user', to: 'users#create'
     delete 'remove_user/:id', to: 'users#destroy', as: :remove_user
